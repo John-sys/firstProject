a597eb8801680b9b543891f2309947dd305e9f22
John-sys <gbayakokoyajohnjr@gmail.com>
2022-05-20 14:33:03 +0000

    new project

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+     {/* <h1 className='text-emerald-400 text-center text-4xl'>Hello world</h1> */}
+    </div>
+  );
+}
+
+export default App;
