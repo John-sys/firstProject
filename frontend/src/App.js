ab39af3ba59380716f90e4f765da35e6b43b5aa5
John-sys <gbayakokoyajohnjr@gmail.com>
2022-05-20 18:20:16 +0000

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@
 function App() {
   return (
     <div className="App">
-     <h1 className='text-emerald-400 text-center text-4xl'>Hello world</h1>
+     <h1 className='text-emerald-400 text-center text-4xl'>Hello Guys</h1>
+     <h2>New world</h2>
     </div>
   );
 }
