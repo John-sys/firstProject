ba14b2dbfde3a4a18bfbc1280d0e04011da9348f
John-sys <gbayakokoyajohnjr@gmail.com>
2022-05-27 14:37:53 +0000

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Homepage from "./views/Homepage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./views/About";
 import Projects from "./views/Projects";
@@ -8,7 +8,7 @@ import Projects from "./views/Projects";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <div className="bg-[#F2F6F9]">
           <Navbar />
 
@@ -18,7 +18,7 @@ function App() {
             <Route path="/projects" element={<Projects />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
